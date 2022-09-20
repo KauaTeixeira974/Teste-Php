15b6c4b2d5ab60cee3a6f149a454148e07d96858
Kauã Teixeira <82388087+KauaTeixeira974@users.noreply.github.com>
2022-09-20 14:29:17 -0300

    Add files via upload

---
+++ media.php
@@ -0,0 +1,29 @@
+O
+script php abaixo carrega 50 números INTEIROS aleatórios em um vetor chamado
+$vet_num, programe uma solução que calcule a média de todos os elementos contidos nele e verifique se esse valor, obtido com o cálculo da média, está presente no vetor e em qual posição (índice) ele se encontra.
+
+
+<?php
+
+for($i = 0; $i < 50; $i++){
+   $vet_num[$i] = rand(0,100);
+
+}
+  
+
+for($i = 0; $i < 50; $i++){
+    
+    if($i <= 50;){
+
+    $i++;
+
+    $vet_num[$i] +$vet_num[$i];
+
+}
+
+    else(){
+
+    $vet_num[$i] / 50;
+
+}        
+?>
